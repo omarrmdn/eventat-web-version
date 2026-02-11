9f67e9a02d1af69c30e14b135a45a6499b3fc3f2
omarrmdn <omarrmdn2024@gmail.com>
2026-02-11 14:53:20 +0200

    done

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,6 @@
 /** @type {import("next").NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  outputFileTracingRoot: new URL("..", import.meta.url).pathname,
-  devIndicators: {
-    buildActivity: false,
-    appIsrStatus: false,
-  },
 };
 
 export default nextConfig;
